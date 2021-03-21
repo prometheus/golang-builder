29d69a51b3faab3d04a4d60b5c2ba209d75efe57
Ben Kochie <superq@gmail.com>
2021-03-21 09:00:30 +0100

    Add universal CGO build image.
    Make a single Docker image for all builds.
    * Merge crossbuild packages into one Dockerfile.
    * Update builder script to support all CGO crossbuilds.
    * Add tags to point to main image for compatibility.
    * Use crossbuild for 386 CGO builds.
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- 1.16/main/rootfs/builder.sh
+++ 1.16/main/rootfs/builder.sh
@@ -23,25 +23,59 @@ for goarch in "${goarchs[@]}"
 do
   goos=${goarch%%/*}
   arch=${goarch##*/}
+  goarm='' cc='gcc' cxx='g++' extra_args=''
+
+  case "${goos}" in
+    windows)
+      case "${arch}" in
+        386) cc='i686-w64-mingw32-gcc' cxx='i686-w64-mingw32-g++' ;;
+        amd64) cc='x86_64-w64-mingw32-gcc' cxx='x86_64-w64-mingw32-g++' ;;
+      esac
+      ;;
+    darwin)
+      case "${arch}" in
+        amd64) cc='o64-clang' cxx='o64-clang++' extra_args='LD_LIBRARY_PATH=/usr/osxcross/lib' ;;
+        arm64) cc='oa64-clang' cxx='oa64-clang++' extra_args='LD_LIBRARY_PATH=/usr/osxcross/lib' ;;
+      esac
+      ;;
+    linux)
+      case "${arch}" in
+        386) cc='i686-linux-gnu-gcc' cxx='i686-linux-gnu-g++' ;;
+        arm64) cc='aarch64-linux-gnu-gcc' cxx='aarch64-linux-gnu-g++' ;;
+        armv*) goarm="${arch##*v}"
+          if [[ "${goarm}" == "7" ]]; then
+            cc='arm-linux-gnueabihf-gcc' cxx='arm-linux-gnueabihf-g++'
+          else
+            cc='arm-linux-gnueabi-gcc' cxx='arm-linux-gnueabi-g++'
+          fi
+          ;;
+        mips) cc='mips-linux-gnu-gcc' cxx='mips-linux-gnu-g++' ;;
+        mips64) cc='mips64-linux-gnuabi64-gcc' cxx='mips64-linux-gnuabi64-g++' ;;
+        mipsle) cc='mipsel-linux-gnu-gcc' cxx='mipsel-linux-gnu-g++' ;;
+        mips64le) cc='mips64el-linux-gnuabi64-gcc' cxx='mips64el-linux-gnuabi64-g++' ;;
+        ppc64) cc='powerpc-linux-gnu-gcc' cxx='powerpc-linux-gnu-g++' ;;
+        ppc64le) cc='powerpc64le-linux-gnu-gcc' cxx='powerpc64le-linux-gnu-g++' ;;
+        s390x) cc='gcc-s390x-linux-gnu' cxx='g++-s390x-linux-gnu' ;;
+      esac
+      ;;
+    netbsd)
+      case "${arch}" in
+        386) cc='i686-linux-gnu-gcc' cxx='i686-linux-gnu-g++' ;;
+      esac
+      ;;
+  esac
 
   echo "# ${goos}-${arch}"
   prefix=".build/${goos}-${arch}"
   mkdir -p "${prefix}"
 
-  if [[ "${goos}" == "windows" ]]; then
-    if [[ "${arch}" == "386" ]]; then
-      CC="i686-w64-mingw32-gcc" CXX="i686-w64-mingw32-g++" GOOS=${goos} GOARCH=${arch} make PREFIX="${prefix}" build
-    else
-      CC="x86_64-w64-mingw32-gcc" CXX="x86_64-w64-mingw32-g++" GOOS=${goos} GOARCH=${arch} make PREFIX="${prefix}" build
-    fi
-  elif [[ "${goos}" == "darwin" ]]; then
-    if [[ "${arch}" == "386" ]]; then
-      CC="o32-clang" CXX="o32-clang++" GOOS=${goos} GOARCH=${arch} make PREFIX="${prefix}" build
-    else
-      CC="o64-clang" CXX="o64-clang++" GOOS=${goos} GOARCH=${arch} make PREFIX="${prefix}" build
-    fi
+  echo "# Building with: CC='${cc}' CXX='${cxx}'"
+  if [[ -n "${goarm}" ]]; then
+    CC="${cc}" CXX="${cxx}" GOOS="${goos}" GOARCH="${arch}" GOARM="${goarm}" \
+      make PREFIX="${prefix}" build ${extra_args}
   else
-    GOOS=${goos} GOARCH=${arch} make PREFIX="${prefix}" build
+    CC="${cc}" CXX="${cxx}" GOOS="${goos}" GOARCH="${arch}" \
+      make PREFIX="${prefix}" build ${extra_args}
   fi
 done
 
